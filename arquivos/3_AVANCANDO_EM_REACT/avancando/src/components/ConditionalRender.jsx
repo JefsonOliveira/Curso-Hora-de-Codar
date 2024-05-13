18e42180d5b7f82b3de1d6be289ea46974effae4
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-05-13 16:15:31 -0300

    feat: Adição ConditionalRender

--- arquivos/3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.jsx
+++ arquivos/3_AVANCANDO_EM_REACT/avancando/src/components/ConditionalRender.jsx
@@ -1,10 +1,23 @@
 const ConditionalRender = () => {
   const x = true;
 
+  const name = "Matheus";
+
   return (
     <div>
       {/* 7 - render condicional */} <h3>Isso será exibido</h3>
       {x && <p>Se x for true sim!</p>}
+      {/* 8 - else */}
+      <h3>Render ternario</h3>
+      {name === "João" ? (
+        <div>
+            <p>Olá João!</p>
+        </div>
+      ) : (
+        <div>
+            <p>Nome não encontrado!</p>
+        </div>
+      )}
     </div>
   );
 };
