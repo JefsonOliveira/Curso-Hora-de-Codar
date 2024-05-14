ff1968acd38e22d0328d7c430a2e52e87558c171
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-05-14 16:05:03 -0300

    feat: estilizado calculadora imc

---
+++ arquivos/imc_calc/src/data/data.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+        min: 0,
+        max: 18.4,
+        classification: "Menor que 18,5",
+        info: "Magreza",
+        obesity: "0",
+        infoClass: "medium"
+    },
+    {
+        min: 18.5,
+        max: 24.9,
+        classification: "Entre 18,5 e 24,9",
+        info: "Normal",
+        obesity: "0",
+        infoClass: "good",
+    },
+    {
+        min: 25,
+        max: 29.9,
+        classification: "Entre 25 e 29,9",
+        info: "Sobrepeso",
+        obesity: "I",
+        infoClass: "medium"
+    },
+    {
+        min: 30,
+        max: 34.9,
+        classification: "Entre 30 e 34,9",
+        info: "Obesidade grau I",
+        obesity: "II",
+        infoClass: "bad"
+    },
+    {
+        min: 35,
+        max: 39.9,
+        classification: "Entre 35 e 39,9",
+        info: "Obesidade grau II (severa)",
+        obesity: "III",
+        infoClass: "bad"
+    },
+    {
+        min: 40,
+        max: Infinity,
+        classification: "Maior ou igual a 40",
+        info: "Obesidade grau III (mórbida)",
+        obesity: "IV",
+        infoClass: "very-bad"
+    }
+];
