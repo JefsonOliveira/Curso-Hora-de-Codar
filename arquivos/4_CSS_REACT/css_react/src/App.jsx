0aca17d01e52158f3ee46cebfc5d9f53dc00fa41
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-05-14 11:15:52 -0300

    feat: Adição ChangeMessage

---
+++ arquivos/4_CSS_REACT/css_react/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* 1 - Css global */}
+      <h1>CSS no React</h1>
+    </div>
+  );
+}
+
+export default App;
