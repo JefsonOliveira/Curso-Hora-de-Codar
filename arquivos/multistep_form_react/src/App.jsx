8676efbd9baedd8d18289a55ca335de018d80f40
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-05-17 14:28:05 -0300

    feat: Finalizado projeto de Form multistep

--- arquivos/multistep_form_react/src/App.jsx
+++ arquivos/multistep_form_react/src/App.jsx
@@ -1,9 +1,79 @@
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { FiSend } from "react-icons/fi";
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
+
+//  Hooks
+import { useForm } from "./hooks/useForm";
+import { useState } from "react";
+
 import "./App.css";
 
+const formTemplate = {
+  name: "",
+  email: "",
+  review: "",
+  comment: "",
+};
+
 function App() {
+  const [data, setData] = useState(formTemplate);
+
+  const updateFieldHandler = (key, value) => {
+    setData((prev) => {
+      return { ...prev, [key]: value };
+    });
+  };
+
+  // eslint-disable-next-line react/jsx-key
+  const formComponents = [
+    // eslint-disable-next-line react/jsx-key
+    <UserForm data={data} updateFieldHandler={updateFieldHandler} />,
+    // eslint-disable-next-line react/jsx-key
+    <ReviewForm data={data} updateFieldHandler={updateFieldHandler} />,
+    // eslint-disable-next-line react/jsx-key
+    <Thanks data={data} />,
+  ];
+
+  // eslint-disable-next-line no-unused-vars
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } =
+    useForm(formComponents);
   return (
-    <div className="App">
-      <h1>React Multistep</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <Steps currentStep={currentStep} />
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+          <div className="inputs-container"> {currentComponent} </div>
+          <div className="actions">
+            {!isFirstStep && (
+              <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+            )}
+            {!isLastStep ? (
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
