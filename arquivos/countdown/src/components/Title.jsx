ba40694f5c70eeb066b52a089f703dd852773441
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-05-23 15:34:42 -0300

    feat: Conclusão da sessão Contador Regressivo -ReactJS-

--- arquivos/countdown/src/components/Title.jsx
+++ arquivos/countdown/src/components/Title.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 import "./Title.css";
 
-const Title = ({ title }) => {
-  return <h1 className="title">{title}</h1>;
+const Title = ({ title, eventColor }) => {
+  return <h1 className="title" style={{ color : eventColor}} >{title}</h1>;
 };
 
 export default Title;
