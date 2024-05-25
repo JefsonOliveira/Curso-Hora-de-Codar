9d98e5625f5e7afc727b1b08f77e69ee59137114
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-05-25 16:09:15 -0300

    feat: Até a 10/31 da sessão Party Time  -ReactJS-

--- arquivos/PARTY_TIME/backend/controllers/serviceController.js
+++ arquivos/PARTY_TIME/backend/controllers/serviceController.js
@@ -11,22 +11,74 @@ const ServiceController = {
       };
 
       const response = await ServiceModel.create(service);
-    
-      res.status(201).json({response, msg: "Serviço criado com sucesso!"})
-    
+
+      res.status(201).json({ response, msg: "Serviço criado com sucesso!" });
     } catch (error) {
       console.log(error);
     }
   },
   getAll: async (req, res) => {
     try {
-        const services = await ServiceModel.find();
+      const services = await ServiceModel.find();
 
-        res.json(services);
+      res.json(services);
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-  }
+  },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const service = await ServiceModel.findById(id);
+
+      if (!service) {
+        res.status(404).json({ msg: "Serviço não encontrado." });
+        return;
+      }
+
+      res.json(service);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const service = await ServiceModel.findById(id);
+
+      if (!service) {
+        res.status(404).json({ msg: "Serviço não econtrado." });
+        return;
+      }
+      const deletedService = await ServiceModel.findByIdAndDelete(id);
+
+      res
+        .status(200)
+        .json({ deletedService, msg: "Serviço excluído com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  update: async (req, res) => {
+    const id = req.params.id;
+
+    const service = {
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      image: req.body.image,
+    };
+
+    const updatedService = await ServiceModel.findByIdAndUpdate(id, service);
+
+    if (!updatedService) {
+      res.status(404).json({ msg: "Serviço não encontro." });
+      return;
+    }
+
+    res.status(200).json({ service, msg: "Serviço atualizado com sucesso." });
+  },
 };
 
 module.exports = ServiceController;
