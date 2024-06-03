03d9099d9f3afc3c0296c8fbc8eb75fe6d22f022
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-06-03 15:38:49 -0300

    feat: backend concluido da sessão Moments -Projeto-  -ReactJS-

--- arquivos/Moments/backend/controllers/MemoryController.js
+++ arquivos/Moments/backend/controllers/MemoryController.js
@@ -114,10 +114,60 @@ const updateMemory = async (req, res) => {
   } catch (error) {}
 };
 
+const toggleFavorite = async (req, res) => {
+  try {
+    const memory = await Memory.findById(req.params.id);
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memória não encontrada!" });
+    }
+
+    memory.favorite = !memory.favorite;
+
+    await memory.save();
+
+    res.json({ msg: "Adicionada aos favoritos", memory });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Ocorreu um erro!");
+  }
+};
+
+const addComment = async (req, res) => {
+  try {
+    const { name, text } = req.body;
+
+    if (!name || !text) {
+      return res
+        .status(400)
+        .json({ msg: "Por favor, preencha todos os campos." });
+    }
+
+    const comment = { name, text };
+
+    const memory = await Memory.findById(req.params.id);
+
+    if (!memory) {
+      return res.status(404).json({ msg: "Memória não encontrada!" });
+    }
+
+    memory.comments.push(comment);
+
+    await memory.save();
+
+    res.json({ msg: "Comentário adicionado", memory });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Ocorreu um erro!");
+  }
+};
+
 module.exports = {
   createMemory,
   getMemories,
   getMemory,
   deleteMemory,
   updateMemory,
+  toggleFavorite,
+  addComment,
 };
