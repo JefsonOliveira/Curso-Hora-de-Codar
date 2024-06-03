4153c7e8824295e9800bab44bf1189f6223ea6d9
Jefson Oliveira <kaironoliveira16@gmail.com>
2024-06-03 16:24:54 -0300

    feat: ate 15/26 da sessão Moments -FrontEnd- -Projeto-  -ReactJS-

---
+++ arquivos/Moments/frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+import { Outlet } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ arquivos/Moments/frontend/src/routes/AddMemory.jsx
@@ -0,0 +1,9 @@
+const AddMemory = () => {
+  return (
+    <div>
+      <h1>Memory</h1>
+    </div>
+  );
+};
+
+export default AddMemory;
